51a266a897c2073dd46ad32be6bfe222ff8d461c
Himvan <47419960+Himvan@users.noreply.github.com>
2019-02-27 09:35:17 +0530

    Update dynm.js

--- dynm.js
+++ dynm.js
@@ -1,30 +1,30 @@
 var data=[ 
     {
-        "F_name":'himvan',
-        "L_Name":'Sharma',
-        "EMAIL":'Himvan.sharma@outlook.com',
-         "E-ID":6164,
-        "MOBILE_NUMBER":7455990114,
-        "DESIGNATION":'Software Engineer',
-        "D.O.B":'18/11/1997'
+        F_name:'Himvan',
+        L_Name:'Sharma',
+        EMAIL:'Himvan.sharma@outlook.com',
+        E_ID:6164,
+        MOBILE_NUMBER:7455990114,
+        DESIGNATION:'Software Engineer',
+        DOB:'18/11/1997'
     },
     {
-        "F_name":'Shivank',
-        "L_Name":'Singh',
-        "EMAIL":'Shivank.singh@outlook.com',
-        "E-ID":6189,
-        "MOBILE_NUMBER":9914408228,
-        "DESIGNATION":'Software Engineer',
-        "D.O.B":'21/02/1997'
+        F_name:'Shivank',
+        L_Name:'Singh',
+        EMAIL:'Shivank.singh@outlook.com',
+        E_ID:6189,
+        MOBILE_NUMBER:9914408228,
+        DESIGNATION:'Software Engineer',
+        DOB:'21/02/1997'
     },
     {
-        "F_name":'Charu',
-        "L_Name":'Chaudhary',
-        "EMAIL":'Charu.chaudhary@outlook.com',
-        "E-ID":6157,
-        "MOBILE_NUMBER":9988456373,
-        "DESIGNATION":'Software Engineer',
-        "D.O.B":'01/11/1996'
+        F_name:'Charu',
+        L_Name:'Chaudhary',
+        EMAIL:'Charu.chaudhary@outlook.com',
+        E_ID:6190,
+        MOBILE_NUMBER:9988456373,
+        DESIGNATION:'Software Engineer',
+        DOB:'01/11/1996'
     }
     ];
 
@@ -38,7 +38,7 @@ function dynamic_table()
         {
             arr.push(Object.values(data[i]));
         }
-        var header=Object.keys(data[0]);
+    var header=Object.keys(data[0]);
     var x=document.createElement("table");
     x.border=1;
     x.setAttribute('id','table_data');
@@ -78,15 +78,74 @@ function dynamic_table()
 function sort(head){
     switch(head) {
         case 0:
-        data.sort()
-
+        
+        data.sort(function(a,b){
+            if(a.F_name<b.F_name)
+              return -1;
+            if(a.F_name>b.F_name)
+               return 1;
+              return 0;
+          });
+       
+        
+      
           break;
         case 1:
-        console.log("in header 1")
+        data.sort(function(a,b){
+            if(a.L_Name<b.L_Name)
+              return -1;
+            if(a.L_Name>b.L_Name)
+               return 1;
+              return 0;
+          });
+         
+         
           break;
-        default:
-        console.log("in header")
-    }
+        case 2:
+        data.sort(function(a,b){
+            if(a.EMAIL<b.EMAIL)
+              return -1;
+            if(a.EMAIL>b.EMAIL)
+               return 1;
+              return 0;
+          });
+      
+         
+         break;
+         case 3:
+         data.sort(function(a,b)
+         {
+             return a.E_ID-b.E_ID;
+         });
+        
+         break;
+         case 4:
+         data.sort(function(a,b)
+         {
+             return a.MOBILE_NUMBER-b.MOBILE_NUMBER;
+         });
+         
+         break;
+         case 5:
+         data.sort(function (a,b){
+            if(a.DESIGNATION<b.DESIGNATION)
+              return -1;
+            if(a.DESIGNATION>b.DESIGNATION)
+               return 1;
+              return 0;
+          });
+       
+        
+         break;
+         default:
+         data.sort(function(a,b)
+         {
+             return a.DOB-b.DOB;
+         });
+        
+   }
+   table_data.remove();
+    dynamic_table();
 
 }
 
